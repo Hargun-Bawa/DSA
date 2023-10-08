1f0fc8d11413b79c93fa17a8a6dd8e4b84c588dc
Matthew Hawksby <103611981+MatthewHawksbyGithub@users.noreply.github.com>
2023-10-08 16:45:10 -0700

    Add files via upload

--- merge.cpp
+++ merge.cpp
@@ -10,7 +10,7 @@ void mergeList(T*& array1, T*& array2, int size1, int size2, T*& array3)
     int k = 0;
 
     while ((i < size1) && (j < size2)) {
-        if (array1[i] > array2[j]) {
+        if (array1[i] >= array2[j]) {
             array3[k++] = array2[j++];
         }
         else {
@@ -23,6 +23,66 @@ void mergeList(T*& array1, T*& array2, int size1, int size2, T*& array3)
     while (j < size2) {
         array3[k++] = array2[j++];
     }
+};
+
+//This function combines two linked lists and stores the result in firstList.
+template <typename T>
+void mergeList(LinkedList<T>& firstList, LinkedList<T>& secondList) {
+    Node<T>* newHead = nullptr;
+
+    //Check if either list is empty and return appropriately
+
+    if (firstList.head == nullptr) {
+        firstList.head = secondList.head;
+        secondList.head = nullptr;
+        return;
+    }
+    if (secondList.head == nullptr) {
+        return;
+    }
+
+    //setup a new head for the merged list.
+
+    if (firstList->head.value > secondList->head.value) {
+        newHead = secondList.head;
+        secondList.head = secondList->head.next;
+    }
+    else {
+        //If both initial nodes have equal values, firstList is the start.
+        newHead = firstList.head;
+        firstList.head = firstList->head.next;
+    }
+
+    //Setup an end pointer for the merged list to keep track while adding nodes.
+
+    Node<T>* endPtr = newHead;
+
+    //Iterate over both linked lists until one of them is empty.
+
+    while ((secondList->head != nullptr) && (firstList->head != nullptr)) {
+        Node<T>* swap = nullptr;
+        if (firstList->head.value <= secondList->head.value) {
+            swap = firstList->head;
+            firstList.head = firstList->head.next;
+        }
+        else {
+            swap = secondList.head;
+            secondList.head = secondList->head.next;
+        }
+        endPtr->next = swap;
+        endPtr = swap;
+    }
+
+    //Add any remaining nodes from the list that isn't empty.
+
+    if (firstList->head != nullptr) {
+        endPtr->next = firstList.head;
+    }
+    else if (secondList->head != nullptr) {
+        endPtr->next = secondList.head;
+    }
+
+    firstList.head = newHead;
 };
 
 int main()
